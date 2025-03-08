8a22410c4cac3ea219c38d455fad15d057fa3c9a
heretic <heretic@kali>
2025-03-08 00:06:21 -0500

    Quartz sync: Mar 8, 2025, 12:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4",
+    pageTitle: "Wifi Forge Docs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
